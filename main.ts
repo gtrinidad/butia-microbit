4c54186908afa27de78f904363fc651dd64c5e25
gtrinidad <gtrinidad@fing.edu.uy>
2026-01-27 11:32:34 -0300

    both motor

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@
 pins.setAudioPin(DigitalPin.P0)
 
 //% color="#84c324" icon="\uf410"
+//% groups="['Sensores', 'Motores']"
 namespace Butia {
     export enum Jconectors {
         //% block="J1"
@@ -26,6 +27,12 @@ namespace Butia {
         //% block="Anti-horario"
         AHorario = 1
     }
+    export enum BDirs {
+        //% block="Adelante"
+        Adelante = 0,
+        //% block="Atras"
+        Atras = 1
+    }
     export enum MOptions {
         //% block="Izquierdo"
         Izquierdo = 0,
@@ -37,6 +44,7 @@ namespace Butia {
      * Este bloque lee un sensor de grises y devuelve su valor
      */
     //% block="Sensor en %pin"
+    //% group="Sensores"
     export function readSensor(pin: Jconectors): number {
         return Math.map(pins.analogReadPin(pin as number as AnalogPin), 0, 1023, 0, 100)
     }
@@ -45,14 +53,31 @@ namespace Butia {
      * Consultar el estado del botón
      */
     //% block="Boton en $pin esta apretado"
+    //% group="Sensores"
     export function readButton(pin: Jconectors): boolean {
         return pins.digitalReadPin(pin as number as AnalogPin) == 0;
     }
 
     /**
-     * Consultar el estado del botón
+     * Mueve todo el robot hacia una direccion
+     */
+    //% block="Mover Butiá hacia $dir"
+    //% group="Motores"
+    export function moveButia(dir: BDirs) {
+        if (dir === BDirs.Adelante) { 
+            moveMotor (MOptions.Izquierdo, RDirs.Horario)
+            moveMotor (MOptions.Derecho, RDirs.AHorario)
+        } else {
+            moveMotor (MOptions.Derecho, RDirs.Horario)
+            moveMotor (MOptions.Izquierdo, RDirs.AHorario)
+        }
+    }
+
+    /**
+     * Mueve un unico motor en una direccion
      */
     //% block="Prender motor $id en sentido $dir"
+    //% group="Motores"
     export function moveMotor(id: MOptions, dir: RDirs) {
         let mDirs = (dir === RDirs.Horario) ? [1, 0] : [0, 1]
         let mPins = (id === MOptions.Izquierdo) ? [DigitalPin.P13,DigitalPin.P14] : [DigitalPin.P15,DigitalPin.P16]
@@ -61,7 +86,6 @@ namespace Butia {
         pins.digitalWritePin(mPins[1], mDirs[1])
     }
 
-    
 
 
 }
