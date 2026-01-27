7aabf29124c192f5c593b142f1daa776379e843c
gtrinidad <gtrinidad@fing.edu.uy>
2026-01-27 10:33:17 -0300

    analog read

--- main.ts
+++ main.ts
@@ -5,11 +5,28 @@
  * generated for an **exported** function.
  */
 
+export enum MyAnalogPin {
+    //% block="J1"
+    J1 = AnalogPin.P1,
+    //% block="J2"
+    J2 = AnalogPin.P2,
+    //% block="J3"
+    J3 = AnalogPin.P3,
+    //% block="J4"
+    J4 = AnalogPin.P4,
+    //% block="J5"
+    J5 = AnalogPin.P5,
+}
+
 //% color="#84c324" icon="\uf410"
 namespace Butia {
-    //% block
-    export function helloWorld() {
-
+    /**
+     * Este bloque lee un sensor de grises y devuelve su valor
+     */
+    
+    //% block="Sensor en %pin"
+    export function readSensor(pin: MyAnalogPin): number {
+        return pins.analogReadPin(pin as AnalogPin)
     }
 
     // note that Caml casing yields lower case
