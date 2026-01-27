fcc868dc9db3aa3abbf6d4b3de244aa69d1dd27e
gtrinidad <gtrinidad@fing.edu.uy>
2026-01-27 11:04:01 -0300

    buzzer conf

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@
  * is "block", and it specifies that a block should be
  * generated for an **exported** function.
  */
+pins.setAudioPin(DigitalPin.P0)
 
 //% color="#84c324" icon="\uf410"
 namespace Butia {
